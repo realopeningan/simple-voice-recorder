37f08f96c597266dd24d7bf740f25af846e8a835
SangukChoi <suchoi@zigbang.com>
2022-11-10 09:10:16 +0900

    Initial Commit (use nextjs + capacitor)

---
+++ packages/app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'simplevoicerecorder.realopeningan.github.io',
+  appName: 'simple-voice-recorder',
+  webDir: '../web/out',
+  bundledWebRuntime: false
+};
+
+export default config;
