76d140b5c7571cecfa58a7a3d702e66db9055b61
SangukChoi <suchoi@zigbang.com>
2022-11-13 18:34:40 +0900

    add for dev prod env

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -1,10 +1,30 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
+const stage = process.env.STAGE
+
+let url= 'http://localhost:3000'
+switch(stage){
+  case 'dev':
+    url = 'http://localhost:3000'
+    break
+  case 'prod':
+    url = 'https://realopeningan.github.io/simple-voice-recorder'
+    break
+  default:
+    console.log("process.env.STAGE", process.env.STAGE)
+    console.log("process.env", process.env)
+    throw new Error('Stage must be one of dev, prod')
+}
+
+
 const config: CapacitorConfig = {
   appId: 'simplevoicerecorder.realopeningan.github.io',
   appName: 'simple-voice-recorder',
   webDir: '../web/out',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+	server: {
+		url,
+	}
 };
 
 export default config;
