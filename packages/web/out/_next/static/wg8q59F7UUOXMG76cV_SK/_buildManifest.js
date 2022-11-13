373e031a8bae4dfb91b9390efab547c5fcfb9c23
SangukChoi <suchoi@zigbang.com>
2022-11-13 09:19:57 +0900

    use capacitor-voice-recorder

---
+++ packages/web/out/_next/static/wg8q59F7UUOXMG76cV_SK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-e65b89dee250f29f.js","static/chunks/pages/index-2e459bb412512ab2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,25 +1,96 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useReactMediaRecorder } from "react-media-recorder";
-import { ReactMediaRecorder } from "react-media-recorder";
-import dynamic from 'next/dynamic';
 import useRecorder from '../hooks/use-recorder';
-import RecorderControls from '../component/recorder-controls'
-import RecordingsList from '../component/recordings-list';
+import {
+  VoiceRecorder,
+  VoiceRecorderPlugin,
+  RecordingData,
+  GenericResponse,
+  CurrentRecordingStatus
+} from 'capacitor-voice-recorder';
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { css } from '@emotion/react'
 
 function Home() {
   const { recorderState, ...handlers } = useRecorder()
-  const {audio} = recorderState
+	const [base64Sound, setBase64Sound] = useState<undefined|string>(undefined)
+
+
+  useEffect(()=>{
+    try{
+      VoiceRecorder.canDeviceVoiceRecord().then(
+        (result: GenericResponse) => console.log(result.value)
+      )
+
+      VoiceRecorder.requestAudioRecordingPermission().then(
+        (result: GenericResponse) => console.log(result.value)
+      )
+
+      VoiceRecorder.hasAudioRecordingPermission().then(
+        (result: GenericResponse) => console.log(result.value)
+      )
+    }catch(e){
+      console.log("error", e)
+    }
+  }, [])
+
+  const startRecording = () =>{
+    VoiceRecorder.startRecording()
+    .then((result: GenericResponse) => console.log(result.value))
+    .catch(error => console.log(error))
+  }
+
+  const stopRecording = () =>{
+    VoiceRecorder.stopRecording()
+    .then((result: RecordingData) => {
+      console.log(result.value)
+      setBase64Sound(result.value.recordDataBase64)
+    })
+    .catch(error => console.log(error))
+  }
+
+  const pauseRecording = () =>{
+    VoiceRecorder.pauseRecording()
+    .then((result: GenericResponse) => console.log(result.value))
+    .catch(error => console.log(error))
+  }
+
+
+  const resumeRecording = () =>{
+    VoiceRecorder.resumeRecording()
+    .then((result: GenericResponse) => console.log(result.value))
+    .catch(error => console.log(error))
+  }
+
+  const getStatus = () =>{
+    VoiceRecorder.getCurrentStatus()
+    .then((result: CurrentRecordingStatus) => console.log(result.status))
+    .catch(error => console.log(error))
+  }
+
+  const playRecording = () =>{
+    if(base64Sound!==undefined){
+      const mimeType = 'audio/aac'  // from plugin
+      const audioRef = new Audio(`data:${mimeType};base64,${base64Sound}`)
+      console.log("base64Sound", base64Sound)
+      audioRef.oncanplaythrough = () => audioRef.play()
+      audioRef.load()
+    }
+  }
 
   return (
-    <section>
+    <div>
       <h1 className='title'>Voice Recorder</h1>
-      <div className='recorder-container'>
-        <RecorderControls recorderState={recorderState} handlers={handlers}/>
-        <RecordingsList audio={audio}/>
-      </div>
-    </section>
+      <Box sx={{ '& button': { m: 1 }, 'display': 'flex', 'flexDirection': 'column'}}>
+        <Button variant="contained" onClick={startRecording}>Recording Start</Button>
+        <Button variant="contained" onClick={stopRecording}>Recording Stop</Button>
+        <Button variant="contained" onClick={pauseRecording}>Recording Pause</Button>
+        <Button variant="contained" onClick={resumeRecording}>Recording Resume</Button>
+        <Button variant="outlined" onClick={playRecording}>Playing</Button>
+        <Button variant="outlined" onClick={getStatus}>GetStatus</Button>
+      </Box>
+    </div>
   )
 }
 
